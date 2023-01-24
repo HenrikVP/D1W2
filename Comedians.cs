719fdd6f64afbd238debd9e8db177fdbbfc9670f
Henrik Vincents Poulsen <hpo@tec.dk>
2023-01-24 09:34:38 +0100

    Add project files.

---
+++ Comedians.cs
@@ -0,0 +1,38 @@
+﻿namespace D1W2
+{
+    internal class Comedians
+    {
+        public static int Age = 10;
+        //Field
+        private string? comedian;
+        //Property
+
+        public int Id { get; set; }
+        public string? Comedian 
+        { 
+            get { return comedian; } 
+            set { comedian = value; } 
+        }
+
+        public Class3 Class34 { get; set; }
+
+        public Class2 Class2 { get; set; }
+        public DateTime BirthDay { get; set; }
+        //Empty Constructor
+        public Comedians()
+        {
+
+        }
+        //Constructor with parameter
+        public Comedians(string comedian, DateTime bd)
+        {
+            this.comedian = comedian;
+            BirthDay = bd;
+        }
+
+        public class Class3
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using D1W2;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            new Program().Start();
+            //Console.WriteLine(cmd.Comedian);
+
+            //new Comedians("Hello Brave New World!");
+        }
+
+        void Start()
+        {
+            Comedians cmd = new Comedians();
+            cmd.Comedian = "Jim Carrey";
+            cmd.BirthDay = new DateTime(1962, 1, 17);
+            ShowComedian(cmd);
+
+            Comedians cmd3 = new();
+            cmd3.Comedian = "Anden";
+            Console.WriteLine(cmd.Comedian);
+
+            Comedians cmd4 = new("Steve Carell", new DateTime(1962, 8, 16));
+
+            Comedians cmd5 = new() {Comedian = "Bill Burr" , BirthDay = new (1968,6,10)};
+        }
+
+        void ShowComedian(Comedians cmd2)
+        {
+            Console.WriteLine(cmd2.Comedian);
+
+            cmd2.Comedian += " is funny";
+        }
+    }
+}
